0eafcd1517d483c70f52a667978cb592dd09087d
Cameron Presley <cameron@thesoftwarementor.com>
2018-01-10 16:50:27 -0500

    Initial creation

---
+++ WindowsService.ps1
@@ -0,0 +1,27 @@
+# Requires admin permissions
+param(
+    [Parameter(Mandatory=$True)]
+    [string]$serviceName
+)
+$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+if ($service -eq $null)
+{
+    Write-Output "The '$serviceName' service does not seem to be installed on $env:ComputerName.$env:UserDomain"
+    Write-Output "Skipping stop/delete steps"
+}
+else {
+    Write-Output "Found the '$serviceName' service."
+    Write-Output "Stopping the '$serviceName' service."
+    Stop-Service -InputObject $service 
+    Write-Output "The '$serviceName' service has been stopped."
+    $service = Get-WmiObject -Class win32_Service -Filter "Name='$serviceName'"
+    Write-Output "Attempting to delete the '$serviceName' service"
+    $result = $service.delete()
+    if ($result.ReturnValue -ne 0){
+        Write-Output "Failed to delete the '$serviceName' service with an error code of $($result.ReturnValue)"
+        Write-Output "Please refer to https://msdn.microsoft.com/en-us/library/aa389960(v=vs.85).aspx for more information on the error code"
+    }
+    else{
+        Write-Output "Successfully deleted the '$serviceName' service"
+    }
+}
